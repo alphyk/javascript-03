747be5e2b50846bb97c74cbe607db0302fca806c
Alphy Kudilil <P15206364@LEC-ADMIN.dmu.ac.uk>
2017-10-19 10:41:09 +0100

    add header and textboxes

--- js/greeting.js
+++ js/greeting.js
@@ -2,6 +2,8 @@
   let myName = document.getElementById("my-name"); 
   let userName = document.getElementById("user-name"); 
   let getName = document.getElementById("get-name");
+  let myHeader = document.getElementById("my-header");
+  let userGreeting = document.getElementById("user-greeting");
   
   function PerformGreeting() {
     myName.innerHTML = userName.value;
